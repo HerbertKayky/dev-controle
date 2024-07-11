5622e0c64b1afdff0af000d02b03d0311a8c546e
HerbertKayky <herbertkayky9@gmail.com>
2024-07-10 21:17:47 -0300

    página home criada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import heroImg from "@/assets/hero.svg";
+
 export default function Home() {
   return (
-    <main className="px-10">
-      <h1 className="text-2xl">Oi</h1>
+    <main className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)]">
+      <h2 className="font-medium text-2xl mb-2">Gerencie sua empresa</h2>
+      <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">
+        Atendimentos, clientes
+      </h1>
+      <Image
+        src={heroImg}
+        alt="Imagem Hero do site"
+        width={600}
+        className="max-w-sm md:max-w-xl"
+      />
     </main>
   );
 }
