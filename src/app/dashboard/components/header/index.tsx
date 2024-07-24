a2b062e16f8bb4a3cc8348efdcfe2b63cd69e725
HerbertKayky <herbertkayky9@gmail.com>
2024-07-24 20:39:05 -0300

    Criado tabela da dashboard

--- src/app/dashboard/components/header/index.tsx
+++ src/app/dashboard/components/header/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function DashboardHeader() {
   return (
     <Container>
-      <header className="w-full bg-gray-900 mt-4 p-3 rounded flex gap-4 items-center">
+      <header className="w-full bg-gray-900 mt-4 mb-3 p-3 rounded flex gap-4 items-center">
         <Link
           className="text-white hover:font-bold duration-300"
           href="/dashboard"
