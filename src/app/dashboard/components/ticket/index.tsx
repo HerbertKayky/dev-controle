0016ed26841e3df2e76c1052188a3be65ca1b9ec
HerbertKayky <herbertkayky9@gmail.com>
2024-08-09 15:25:25 -0300

    listando chamados em aberto

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,14 +1,31 @@
+import { CustomerProps } from "@/utils/customer.type";
+import { TicketProps } from "@/utils/ticket.type";
 import { FiFile, FiTrash2 } from "react-icons/fi";
 
-export function TicketItem() {
+interface TicketItemProps {
+  ticket: TicketProps;
+  customer: CustomerProps | null;
+}
+
+export function TicketItem({ customer, ticket }: TicketItemProps) {
   return (
     <>
-      <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300">
-        <td className="text-left pl-1">Karl Marx</td>
-        <td className="text-left hidden sm:table-cell">02/04/2023</td>
-        <td className="text-left">
-          <span className="bg-green-500 px-2 py-1 rounded">ABERTO</span>
+      <tr
+        className="border-b-2 border-b-slate-200 h-16
+        last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300"
+      >
+        <td className="text-left pl-1">{customer?.name}</td>
+
+        <td className="text-left hidden sm:table-cell">
+          {ticket.created_at?.toLocaleDateString("pt-br")}
         </td>
+
+        <td className="text-left">
+          <span className="bg-green-500 px-2 py-1 rounded">
+            {ticket.status}
+          </span>
+        </td>
+
         <td className="text-left">
           <button className="mr-2">
             <FiTrash2 size={24} color="#EF4444" />
